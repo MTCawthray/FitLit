058f88ce6d926f23727718eb598c69aefa3515d8
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-12 14:49:23 -0600

    Write block tests to create the find user method

--- src/UserRepository.js
+++ src/UserRepository.js
@@ -1,6 +1,11 @@
 class UserRepository {
   constructor(data) {
     this.data = data;
+    console.log(this.data);
+  }
+
+  findUser(id) {
+    return this.data.find(user => user.id === id);
   }
 
 }
--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -3,10 +3,10 @@ import UserRepository from '../src/UserRepository';
 const sampleUserData = require('../src/data/sample-users');
 
 describe('User Repository', () => {
-  let newUsers;
+  let userRepo;
 
   beforeEach(() => {
-    newUsers = new UserRepository(sampleUserData);
+    userRepo = new UserRepository(sampleUserData);
   });
 
     it('should be a function', function() {
@@ -14,8 +14,23 @@ describe('User Repository', () => {
     });
 
     it('should be an instance of a class', () => {
-      expect(newUsers).to.be.an.instanceOf(UserRepository);
+
+      expect(userRepo).to.be.an.instanceOf(UserRepository);
     });
 
-    
+    it('should have a property that holds all user data', () => {
+      expect(userRepo.data).to.deep.equal(sampleUserData);
+    });
+
+    describe('findUser()', () => {
+
+      it('should be a function', () => {
+        expect(userRepo.findUser).to.be.a('function');
+      });
+
+      it('should take in a parameter of userId and return that user\'s data', () => {
+        expect(userRepo.findUser(1)).to.deep.equal(sampleUserData[0]);
+      });
+
+    })
 });
