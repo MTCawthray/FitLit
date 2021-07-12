6d057d26351d15c6f028badefcfcbbc24c3fb673
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-12 14:20:15 -0600

    Write describe block tests to create user respository class

--- test/UserRepository-test.js
+++ test/UserRepository-test.js
@@ -1,8 +1,21 @@
 import { expect } from 'chai';
 import UserRepository from '../src/UserRepository';
+const sampleUserData = require('../src/data/sample-users');
 
 describe('User Repository', () => {
+  let newUsers;
+
+  beforeEach(() => {
+    newUsers = new UserRepository(sampleUserData);
+  });
+
     it('should be a function', function() {
         expect(UserRepository).to.be.a('function');
     });
+
+    it('should be an instance of a class', () => {
+      expect(newUsers).to.be.an.instanceOf(UserRepository);
+    });
+
+    
 });
