bebcfb46e8d5d1519e5800a8ad629e5acebf68b1
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-14 16:02:02 -0600

    Create load friends function to update dom

--- src/DOM-Manipulation.js
+++ src/DOM-Manipulation.js
@@ -1,17 +1,35 @@
-const scripts = require('../src/scripts.js');
-//------query-selectors-------//
 
-
-
-
-//------event-listeners----//
-
-
-
-
-//-------functions------//
-
-
-
-
+//make an object and write functions inside
 //
+const domUpdates = {
+
+  render() {
+    console.log('hi!')
+    //write query selectors in methods
+    //if using same qs in differnt methods, move it to global scope
+  },
+
+  renderStepCard() {
+
+  },
+
+  renderFriendCard(friends) {
+    const friendCard = document.getElementById('friendCard');
+    friendCard.innerHTML = '';
+    friends.forEach(friend => {
+    friendCard.innerHTML +=
+    `  <div class="friend-display">
+    <img src=" " alt="friend icon">
+    <p>${friend}</p>
+    </div>
+    `
+
+  })
+  }
+
+}
+
+
+//export object to call on in scripts.js
+//
+export default domUpdates;
--- src/scripts.js
+++ src/scripts.js
@@ -1,37 +1,38 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
 
-// An example of how you tell webpack to use a CSS file
+
 import './css/styles.css';
-
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
 import './images/turing-logo.png'
-
-console.log('This is the JavaScript entry file - your code begins here.');
-
-// An example of how you tell webpack to use a JS file
-
-// import userData from './data/users';
 import UserRepository from './UserRepository';
 import User from './User';
-
 import userData from '../src/data/users';
+import domUpdates from '../src/DOM-Manipulation'
+
+
+//----event listeners----///
+window.addEventListener('load', loadFriends)
+
 
 //task 1
 //create an info card on the dashboard with name, friends, daily step goal, stride length.
 let userRepo, newUser;
+function sayHello() {
+  console.log('hello');
+}
 
 
-function makeRepo(id) {
-  userRepo = new UserRepository(userData);
+// domUpdates.render();
+//---helper functions----//
+
+function makeRepo(data, id) {
+  userRepo = new UserRepository(data);
   newUser = new User(userRepo.findUser(id));
 }
 
-makeRepo(1);
-console.log(getName(1));
-function getName(id) {
-  makeRepo(id)
-  return newUser.name;
+function getName(data, id) {
+  makeRepo(data, id)
+  const name = newUser.name;
+  domUpdates.renderName(name);
 };
 
 function getFriends(user) {
@@ -42,6 +43,8 @@ function getFriends(user) {
   }, []).map(friend => friend.name);
   return returnName;
 };
+makeRepo(userData, 2);
+// console.log(getStride(newUser));
 
 function getStepGoal(user) {
   return user.dailyStepGoal;
@@ -59,5 +62,11 @@ function getAvgStepGoal(allUsers) {
   return allUsers.findAvgStepGoal();
 }
 
+//---page load functions---//
+function loadFriends() {
+  const friends = getFriends(newUser);
+  domUpdates.renderFriendCard(friends);
+  console.log(friends);
+}
 
 //
