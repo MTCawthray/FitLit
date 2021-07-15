ee356d99bcc2454856cd711053017e5588e384f5
Maria DelSignore <maria.ad27@gmail.com>
2021-07-15 16:32:55 -0600

    Remove unused code

--- src/data/apiCalls.js
+++ src/data/apiCalls.js
@@ -1,32 +1,29 @@
-import { assignVariables, loadUserInfo } from '../src/scripts'
-
 const retrieveUserData = () => {
     return fetch('http://localhost:3001/api/v1/users')
-        .then(response => response.json()).catch(err => console.log('USERS API Error'));
+        .then(response => response.json())
+        .then(data => data)
+        .catch(err => console.log('USERS API Error'));
 };
 
 const retrieveHydrationData = () => {
     return fetch('http://localhost:3001/api/v1/hydration')
         .then(response => response.json())
+        .then(data => data)
         .catch(err => console.log('USERS API Error'));
 };
 
 const retrieveSleepData = () => {
     return fetch('http://localhost:3001/api/v1/sleep')
         .then(response => response.json())
+        .then(data => data)
         .catch(err => console.log('USERS API Error'));
 };
 
 const getData = () => {
-    return Promise.all([retrieveUserData, retrieveHydrationData, retrieveSleepData])
-        .then(data => assignVariables(data))
-        .then(() => loadUserInfo());
+    return Promise.all([retrieveUserData(), retrieveHydrationData(), retrieveSleepData()])
 };
 
 
 export default {
-    retrieveUserData,
-    retrieveHydrationData,
-    retrieveSleepData,
     getData
-};
+}
