19dc7018ccb8d1b864f2604dbe1b079e9d49441c
Maria DelSignore <maria.ad27@gmail.com>
2021-07-14 20:42:31 -0600

    Add promise function

--- src/data/apiCalls.js
+++ src/data/apiCalls.js
@@ -1,3 +1,5 @@
+import { assignVariables, loadUserInfo } from '../src/scripts'
+
 const retrieveUserData = () => {
     return fetch('http://localhost:3001/api/v1/users')
         .then(response => response.json())
@@ -15,3 +17,16 @@ const retrieveSleepData = () => {
         .then(response => response.json())
         .catch(err => console.log('USERS API Error'))
 }
+
+const getData = () => {
+    return Promise.all([retrieveUserData, retrieveHydrationData, retrieveSleepData])
+        .then(data => assignVariables(data))
+        .then(() => loadUserInfo())
+}
+
+
+export default {
+    retrieveUserData,
+    retrieveHydrationData,
+    retrieveSleepData
+};
