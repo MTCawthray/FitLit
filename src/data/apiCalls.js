13f8e44b3089f16953393d6d0729a12c09feed14
Maria DelSignore <maria.ad27@gmail.com>
2021-07-14 21:13:38 -0600

    Add getdata function to export list

--- src/data/apiCalls.js
+++ src/data/apiCalls.js
@@ -2,31 +2,31 @@ import { assignVariables, loadUserInfo } from '../src/scripts'
 
 const retrieveUserData = () => {
     return fetch('http://localhost:3001/api/v1/users')
-        .then(response => response.json())
-        .catch(err => console.log('USERS API Error'))
-}
+        .then(response => response.json()).catch(err => console.log('USERS API Error'));
+};
 
 const retrieveHydrationData = () => {
     return fetch('http://localhost:3001/api/v1/hydration')
         .then(response => response.json())
-        .catch(err => console.log('USERS API Error'))
-}
+        .catch(err => console.log('USERS API Error'));
+};
 
 const retrieveSleepData = () => {
     return fetch('http://localhost:3001/api/v1/sleep')
         .then(response => response.json())
-        .catch(err => console.log('USERS API Error'))
-}
+        .catch(err => console.log('USERS API Error'));
+};
 
 const getData = () => {
     return Promise.all([retrieveUserData, retrieveHydrationData, retrieveSleepData])
         .then(data => assignVariables(data))
-        .then(() => loadUserInfo())
-}
+        .then(() => loadUserInfo());
+};
 
 
 export default {
     retrieveUserData,
     retrieveHydrationData,
-    retrieveSleepData
+    retrieveSleepData,
+    getData
 };
