2758c8a566d16b3d9406a3f531386cd55b94441f
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-15 19:44:40 -0600

    Write describe block tests to create hydration repository and find user method

--- src/HydrationRepository.js
+++ src/HydrationRepository.js
@@ -1,5 +1,13 @@
 class HydrationRepository {
+  constructor(data) {
+    this.data = data;
+  }
 
+  findUser(id) {
+    if (this.data.id === id) {
+      return this.data.find(user => user.id === id);
+    }
+  }
 }
 
 export default HydrationRepository;
--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -1,8 +1,39 @@
 import { expect } from 'chai';
 import HydrationRepository from '../src/HydrationRepository';
+const sampleHydrationData = require('../src/data/sample-hydration');
 
 describe('HydrationRepository', () => {
+
+    let hydrationRepo;
+
+    beforeEach(() => {
+      hydrationRepo = new HydrationRepository(sampleHydrationData);
+    });
+
     it('should be a function', function() {
         expect(HydrationRepository).to.be.a('function');
     });
+
+    it('should be an instance of a class', () => {
+      expect(hydrationRepo).to.be.an.instanceOf(HydrationRepository);
+    });
+
+    it('should hold hydration data', () => {
+      expect(hydrationRepo.data).to.deep.equal(sampleHydrationData);
+    });
+
+    describe('findUser()', () => {
+
+        it('should be a function', () => {
+          expect(hydrationRepo.findUser).to.be.a('function');
+        });
+
+        it('should return a specified user', () => {
+          expect(hydrationRepo.findUser(1)).to.deep.equal(sampleHydrationData[0]);
+          expect(hydrationRepo.findUser(2)).to.deep.equal(sampleHydrationData[1]);
+        });
+    });
+
+    
+
 });
