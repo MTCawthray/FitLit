43844393a55a0b5a8b5dcc186848b78b1841830e
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-16 14:49:37 -0600

    Write describe block tests to create hydration class

--- src/Hydration.js
+++ src/Hydration.js
@@ -1,5 +1,9 @@
 class Hydration {
-
+  constructor(id, date) {
+    this.userID = id;
+    this.date = date;
+    this.numOunces = null;
+  }
 }
 
 export default Hydration;
--- test/Hyration-test.js
+++ test/Hyration-test.js
@@ -1,8 +1,31 @@
 import { expect } from 'chai';
+import HydrationRepository from '../src/HydrationRepository';
 import Hydration from '../src/Hydration';
+const sampleHydrationData = require('../src/data/sample-hydration');
+
 
 describe('Hydration', () => {
-    it('should be a function', function() {
-        expect(Hydration).to.be.a('function');
+
+  let hydrationRepo, userHydration;
+
+  beforeEach(() => {
+    hydrationRepo = new HydrationRepository(sampleHydrationData);
+    userHydration = new Hydration(2, "2019/06/15");
+  });
+
+    it('should be a function', () => {
+      expect(Hydration).to.be.a('function');
     });
+
+    it('should be an instance of the hydtration class', () => {
+      expect(userHydration).to.be.an.instanceOf(Hydration);
+    });
+
+    it('should hold user data', () => {
+      expect(userHydration.userID).to.equal(2);
+      expect(userHydration.date).to.equal("2019/06/15")
+      expect(userHydration.numOunces).to.equal(null);
+    });
+
+
 });
