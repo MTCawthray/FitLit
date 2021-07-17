c380a2e21406c2920519d3d05e72fef17bfd5213
Maria DelSignore <maria.ad27@gmail.com>
2021-07-17 13:02:52 -0600

    Create a method to interpolate todays ounces on page

--- src/DOM-Manipulation.js
+++ src/DOM-Manipulation.js
@@ -1,50 +1,53 @@
-
 //make an object and write functions inside
 //
 const domUpdates = {
-
-  renderUserName(name) {
-    const user = document.getElementById('nameAvatar');
-    user.innerHTML = '';
-    user.innerHTML += `
+	// user
+	renderUserName(name) {
+		const user = document.getElementById('nameAvatar');
+		user.innerHTML = '';
+		user.innerHTML += `
     <img src=" " alt="user icon">
     <h4>${name}</h4>
     `;
-  },
+	},
 
-  renderFriendCard(friends) {
-    const friendCard = document.getElementById('friendCard');
-    friendCard.innerHTML = '';
-    friends.forEach(friend => {
-    friendCard.innerHTML +=
-      `<div class="friend-display">
+	renderFriendCard(friends) {
+		const friendCard = document.getElementById('friendCard');
+		friendCard.innerHTML = '';
+		friends.forEach((friend) => {
+			friendCard.innerHTML += `<div class="friend-display">
       <img src=" " alt="friend icon">
       <p>${friend}</p>
       </div>
       `;
-    });
-  },
+		});
+	},
 
-  renderStepAnnouncements(goal, avgGoal, stride) {
-    const goals = document.getElementById('stepAnnouncements') ;
-    goals.innerHTML = '';
-    goals.innerHTML += `
+	renderStepAnnouncements(goal, avgGoal, stride) {
+		const goals = document.getElementById('stepAnnouncements');
+		goals.innerHTML = '';
+		goals.innerHTML += `
     <h3>Daily Step Goal</h3>
     <p class=" steps-data">${goal}</p>
     <h3>Avg User Step Goal</h3>
     <p class="steps-data">${avgGoal}</p>
     <h3>Stride Length</h3>
     <p class="steps-data">${stride}</p>
-    `
-  },
+    `;
+	},
 
-  renderWelcome(firstName) {
-    const welcomeArea = document.getElementById('welcomeMessage');
-    welcomeArea.innerText = `Welcome, ${firstName}!`;
-  }
+	renderWelcome(firstName) {
+		const welcomeArea = document.getElementById('welcomeMessage');
+		welcomeArea.innerText = `Welcome, ${firstName}!`;
+	},
 
-}
-
+	// hydration
+	renderTodaysOunces(ounces) {
+		const ouncesDrank = document.getElementById('ouncesDrank');
+		ouncesDrank.innerText = `${ounces}`;
+	},
+	renderWeeklyOunces() {},
+};
 
 //export object to call on in scripts.js
 //
