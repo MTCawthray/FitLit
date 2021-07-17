7fa9b48c7cf6e129971c530155f8920cb2d600da
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-17 17:23:04 -0600

    Create render weekly sleep info method

--- src/DOM-Manipulation.js
+++ src/DOM-Manipulation.js
@@ -67,11 +67,54 @@ const domUpdates = {
 		const todaySleepCard = document.getElementById('todayCard');
 		todaySleepCard.innerHTML = '';
 		todaySleepCard.innerHTML += `
-			<h3>Today</h3>
-			<h3>Hours Slept</h3>
-			<p>${hours}</p>
-			<h3>Sleep Quality</h3>
-			<p>${quality}</p>`;
+			<article class="today-card">
+				<h3>Today</h3>
+				<h3>Hours Slept</h3>
+				<p>${hours}</p>
+				<h3>Sleep Quality</h3>
+				<p>${quality}</p>
+			</article>`;
+	},
+
+	renderWeeklySleepInfo(hours, quality) {
+		const weeklySleepCards = document.getElementById('weeklySleepCards');
+		weeklySleepCards.innerHTML = '';
+		weeklySleepCards.innerHTML += `
+			<article>
+				<h6>Mon</h6>
+				<p>${hours[0]}</p>
+				<p>${quality[0]}</p>
+			</article>
+			<article>
+				<h6>Tue</h6>
+				<p>${hours[1]}</p>
+				<p>${quality[1]}</p>
+			</article>
+			<article>
+				<h6>Wed</h6>
+				<p>${hours[2]}</p>
+				<p>${quality[2]}</p>
+			</article>
+			<article>
+				<h6>Thu</h6>
+				<p>${hours[3]}</p>
+				<p>${quality[3]}</p>
+			</article>
+			<article>
+				<h6>Fri</h6>
+				<p>${hours[4]}</p>
+				<p>${quality[4]}</p>
+			</article>
+			<article>
+				<h6>Sat</h6>
+				<p>${hours[5]}</p>
+				<p>${quality[5]}</p>
+			</article>
+			<article>
+				<h6>Sun</h6>
+				<p>${hours[6]}</p>
+				<p>${quality[6]}</p>
+			</article>`
 	}
 
 };
