529fcece83dd3c3b890d4de65f7e8186dfdbfa5a
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-18 19:14:37 -0600

    Create a chart variable assigned to an instance of chart class

--- src/Chart.js
+++ src/Chart.js
@@ -1,24 +1,22 @@
-class Chart {
-  constructor(node, weeklyOunces) {
-    this.type = 'horizontalBar';
-    this.data = {
-      labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-      dataSets: [
-              {
-                label: '',
-                backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f','#e8c3b9','#c45850'],
-                data: weeklyOunces
-              }
-            ]
-          };
-    this.options = {
-        legend: { display: false },
-        title: {
-          display: true,
-          text: 'weekly ounces drank'
-        }
+const chart = new Chart(document.getElementById('barChartHorizontal'), {
+  type: 'horizontalBar',
+  data: {
+    labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+    datasets: [
+      {
+        label: 'weekly ounces drank',
+        backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f','#e8c3b9','#c45850', '#69D53E', '#69D53E'],
+        data: weeklyOunces //We need to figure out how to connect our weeklyOuncesData to this when the chart lives in it's own file!
       }
+    ]
+  },
+  options: {
+    legend: { display: false },
+    title: {
+      display: true,
+      text: ''
     }
   }
-  
-export default Chart;
+});
+
+export default chart;
