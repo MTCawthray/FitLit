4cedde5a69555b4d8ac307542d59838ea4a90ace
Maria DelSignore <maria.ad27@gmail.com>
2021-07-19 19:13:47 -0600

    Update bar colors in horizontal graph

--- src/Chart.js
+++ src/Chart.js
@@ -1,52 +1,61 @@
 const chart = {
-  horizontalBar: (weeklyOunces) => {
-    return new Chart(document.getElementById('barChartHorizontal'), {
-      type: 'horizontalBar',
-      data: {
-        labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-        datasets: [
-          {
-            label: 'weekly ounces drank',
-            backgroundColor: ['#3e95cd', '#8e5ea2','#3cba9f','#e8c3b9','#c45850', '#69D53E', '#69D53E'],
-            data: weeklyOunces
-          }
-        ]
-      },
-      options: {
-        legend: { display: false },
-        title: {
-          display: true,
-          text: ''
-        }
-      }
-    });
-  },
+	horizontalBar: (weeklyOunces) => {
+		return new Chart(document.getElementById('barChartHorizontal'), {
+			type: 'horizontalBar',
+			data: {
+				labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+				datasets: [
+					{
+						label: 'weekly ounces drank',
+						backgroundColor: [
+							'#4CC9F0',
+							'#4CC9F0',
+							'#4CC9F0',
+							'#4CC9F0',
+							'#4CC9F0',
+							'#4CC9F0',
+							'#4CC9F0',
+						],
+						data: weeklyOunces,
+					},
+				],
+			},
+			options: {
+				legend: { display: false },
+				title: {
+					display: true,
+					text: '',
+				},
+			},
+		});
+	},
 
-  groupedBar: (hours, quality) => {
-    return new Chart(document.getElementById("barChartGrouped"), {
-      type: 'bar',
-      data: {
-        labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
-        datasets: [
-          {
-            label: 'hours slept',
-            backgroundColor: '#3e95cd',
-            data: hours
-          }, {
-            label: 'sleep quality',
-            backgroundColor: '#8e5ea2',
-            data: quality
-          }
-        ]
-      },
-      options: {
-        title: {
-          display: false,
-          text: ''
-        }
-      }
-    });
-  }
-}
+	groupedBar: (hours, quality) => {
+		return new Chart(document.getElementById('barChartGrouped'), {
+			type: 'bar',
+			data: {
+				labels: ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'],
+				datasets: [
+					{
+						label: 'hours slept',
+						backgroundColor: '#a1c181',
+						data: hours,
+					},
+					{
+						label: 'sleep quality',
+						backgroundColor: '#06d6a0',
+						data: quality,
+					},
+				],
+			},
+			options: {
+				title: {
+					display: false,
+					text: '',
+				},
+			},
+		});
+	},
+};
 
 export default chart;
