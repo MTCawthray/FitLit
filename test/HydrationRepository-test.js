a29e76a684a425348969dcd75752782d398d0e2c
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-20 18:08:56 -0600

    Fix linter issues

--- test/HydrationRepository-test.js
+++ test/HydrationRepository-test.js
@@ -3,52 +3,47 @@ import HydrationRepository from '../src/HydrationRepository';
 const sampleHydrationData = require('../src/data/sample-hydration');
 
 describe('HydrationRepository', () => {
+  let hydrationRepo;
 
-    let hydrationRepo;
+  beforeEach(() => {
+    hydrationRepo = new HydrationRepository(sampleHydrationData);
+  });
 
-    beforeEach(() => {
-      hydrationRepo = new HydrationRepository(sampleHydrationData);
+  it('should be a function', function() {
+    expect(HydrationRepository).to.be.a('function');
+  });
+
+  it('should be an instance of a class', () => {
+    expect(hydrationRepo).to.be.an.instanceOf(HydrationRepository);
+  });
+
+  it('should hold hydration data', () => {
+    expect(hydrationRepo.data).to.deep.equal(sampleHydrationData);
+  });
+
+  describe('findUser()', () => {
+    it('should be a function', () => {
+      expect(hydrationRepo.findUser).to.be.a('function');
     });
 
-    it('should be a function', function() {
-        expect(HydrationRepository).to.be.a('function');
+    it('should return a specified user', () => {
+      let specifiedUser1 = sampleHydrationData.filter(sample => sample.userID === 1);
+      expect(hydrationRepo.findUser(1)).to.deep.equal(specifiedUser1);
+    });
+  });
+
+  describe('findDate()', () => {
+    it('should be a function', () => {
+      expect(hydrationRepo.findDate).to.be.a('function');
     });
 
-    it('should be an instance of a class', () => {
-      expect(hydrationRepo).to.be.an.instanceOf(HydrationRepository);
+    it('should return an array of users on a specified date', () => {
+      const foundDate = [
+        { userID: 1, date: '2019/06/15', numOunces: 37 },
+        { userID: 2, date: '2019/06/15', numOunces: 75 },
+        { userID: 3, date: '2019/06/15', numOunces: 47 }
+      ];
+      expect(hydrationRepo.findDate("2019/06/15")).to.deep.equal(foundDate);
     });
-
-    it('should hold hydration data', () => {
-      expect(hydrationRepo.data).to.deep.equal(sampleHydrationData);
-    });
-
-    describe('findUser()', () => {
-
-        it('should be a function', () => {
-          expect(hydrationRepo.findUser).to.be.a('function');
-        });
-
-        it('should return a specified user', () => {
-          let specifiedUser1 = sampleHydrationData.filter(sample => sample.userID === 1);
-          expect(hydrationRepo.findUser(1)).to.deep.equal(specifiedUser1);
-    });
-});
-
-    describe('findDate()', () => {
-
-      it('should be a function', () => {
-        expect(hydrationRepo.findDate).to.be.a('function');
-      });
-
-      it('should return an array of users on a specified date', () => {
-        const foundDate = [
-          { userID: 1, date: '2019/06/15', numOunces: 37 },
-          { userID: 2, date: '2019/06/15', numOunces: 75 },
-          { userID: 3, date: '2019/06/15', numOunces: 47 }
-        ]
-
-        expect(hydrationRepo.findDate("2019/06/15")).to.deep.equal(foundDate);
-      });
-    });
-
+  });
 });
