6aa1baf2f5daf6355ba284932db5ff54ece32d6b
Mark Cawthray <dr.cawthray@gmail.com>
2021-07-20 18:17:25 -0600

    Fix linter issues

--- src/scripts.js
+++ src/scripts.js
@@ -11,7 +11,6 @@ import HydrationRepository from './HydrationRepository';
 import Hydration from './Hydration';
 import SleepRepository from './SleepRepository';
 import Sleep from './Sleep';
-// import chart from 'chart.js/auto'; //We are working on how to pass our weeklyOuncesData to this file..
 import domUpdates from '../src/DOM-Manipulation';
 import apiCalls from '../src/data/apiCalls';
 
@@ -23,7 +22,6 @@ window.addEventListener('load', () => {
 });
 
 //---page load functions---//
-
 function loadApiCalls() {
 	apiCalls.getData().then((promise) => {
 		let userData = promise[0].userData;
@@ -38,7 +36,7 @@ function loadApiCalls() {
 		userSleep = new Sleep(newUser.id, '2019/06/28', sleepRepo);
 		loadUserInfo();
 	});
-}
+};
 
 // user
 function loadUserInfo() {
@@ -51,67 +49,65 @@ function loadUserInfo() {
 	displayTodaysSleepCard();
 	displayWeeklySleepInfo();
 	displayUserAvgSleepInfo();
-}
+};
 
 function displayWelcome(user) {
 	const firstName = getFirstName(user);
 	domUpdates.renderWelcome(firstName);
-}
+};
 
 function displayName(user) {
 	const name = getName(user);
 	domUpdates.renderUserName(name);
-}
+};
 
 function displayFriends(user) {
 	const friends = getFriends(user);
 	domUpdates.renderFriendCard(friends);
-}
+};
 
 function displayStepAnnouncements(user, data) {
 	const goal = getStepGoal(user);
 	const stride = getStride(user);
 	const avgGoal = data.findAvgStepGoal();
 	domUpdates.renderStepAnnouncements(goal, avgGoal, stride);
-}
+};
 
 // hydration
 function displayTodaysOunces() {
 	const ounces = getTodaysOunces();
 	domUpdates.renderTodaysOunces(ounces);
-}
+};
 
 function displayWeeklyOunces(chart) {
 	const weeklyOuncesData = getWeeklyOunces();
 	domUpdates.renderWeeklyOunces(weeklyOuncesData);
-}
+};
 
 // sleep
-
 function displayTodaysSleepCard() {
 	const hoursSlept = getHoursSlept();
 	const sleepQuality = getSleepQuality();
 	domUpdates.renderTodaysSleepCard(hoursSlept, sleepQuality);
-}
+};
 
 function displayWeeklySleepInfo() {
 	const weeklyHoursSlept = getWeeklyHoursSlept();
 	const weeklyQuality = getWeeklyQuality();
 	domUpdates.renderWeeklySleepInfo(weeklyHoursSlept, weeklyQuality);
-}
+};
 
 function displayUserAvgSleepInfo() {
 	const avgHours = getAvgHours();
 	const avgQuality = getAvgQuality();
 	domUpdates.renderUserAvgSleepInfo(avgHours, avgQuality);
-}
+};
 
 //---helper functions----//
-
-// user functions
+// user
 function getName(user) {
 	return user.name;
-}
+};
 
 function getFriends(user) {
 	let userFriends = user.friends;
@@ -122,58 +118,50 @@ function getFriends(user) {
 		}, [])
 		.map((friend) => friend.name);
 	return returnName;
-}
+};
 
 function getStepGoal(user) {
 	return user.dailyStepGoal;
-}
+};
 
 function getStride(user) {
 	return user.strideLength;
-}
+};
 
 function getFirstName(user) {
 	return user.findFirstName();
-}
+};
 
-// **being handled by method in displayStepAnnouncements
-// function getAvgStepGoal(allUsers) {
-// 	return allUsers.findAvgStepGoal();
-// }
-
-// hydration functions
+// hydration
 function getTodaysOunces() {
 	return userHydration.findOuncesByDate();
-}
+};
 
 function getWeeklyOunces() {
 	return userHydration.findWeeklyOunces();
-}
+};
 
-// sleep functions
-
+// sleep
 function getHoursSlept() {
 	return userSleep.findHours();
-}
+};
 
 function getSleepQuality() {
 	return userSleep.findQuality();
-}
+};
 
 function getWeeklyHoursSlept() {
 	return userSleep.findWeeklyHours();
-}
+};
 
 function getWeeklyQuality() {
 	return userSleep.findWeeklyQuality();
-}
+};
 
 function getAvgHours() {
 	return userSleep.calculateAvgHours();
-}
+};
 
 function getAvgQuality() {
 	return userSleep.calculateAvgQuality();
-}
-
-//
+};
